de3a3e93f298df11997fd7553e78bd3f64f535b9
Junsouk Choi <jchoi@stat.tamu.edu>
2019-11-21 20:10:12 -0600

    Create the ZIPBNfunctions.R file with a skeleton of the mcmc_ZIPBN function which implements a MCMC sampler for ZIPBN

---
+++ R/ZIPBNfunctions.R
@@ -0,0 +1,64 @@
+mcmc_ZIPBN = function(x, starting, tuning, priors, n.samples)
+{
+   # store the sample size and the number of variables (nodes)
+   n = nrow(x)
+   p = ncol(x)
+   
+   # initialize parameters with starting values supplied
+   alpha = starting$alpha
+   beta  = starting$beta
+   delta = starting$delta
+   gamma = starting$gamma
+   tau   = starting$tau
+   rho   = starting$rho
+   
+   # set precisions with supplied values for the Metropolis sampler Normal proposal distributions 
+   phi_alpha = tuning$phi_alpha
+   phi_beta  = tuning$phi_beta
+   phi_delta = tuning$phi_delta
+   phi_gamma = tuning$phi_gamma
+   
+   # set hyperparameters with supplied values
+   nu = priors$nu
+   b  = priors$b
+   c  = priors$c
+   
+   # initialize MCMC samples
+   A_MCMC     = array(NA, dim = c(p, p, n.samples))
+   alpha_MCMC = array(NA, dim = c(p, p, n.samples))
+   beta_MCMC  = array(NA, dim = c(p, p, n.samples))
+   delta_MCMC = matrix(NA, p, n.samples)
+   gamma_MCMC = matrix(NA, p, n.samples)
+   tau_MCMC   = matrix(NA, 4, n.samples)
+   rho_MCMC   = rep(NA, n.samples)
+   
+   # do MCMC iterations
+   for (t in 1 : n.samples)
+   {
+      # update alpha (Metropolis-Hastings step)
+      # update beta (Metropolis-Hastings step)
+      # update delta (Metropolis-Hastings step)
+      # update gamma (Metropolis-Hastings step)
+      # update A (Metropolis-Hastings step)
+      # update tau (Gibbs sampling)
+      # update rho (Gibbs sampling)
+      
+      # store MCMC samples of iteration t
+      alpha_MCMC[ , , t] = alpha
+      beta_MCMC[ , , t]  = beta
+      delta_MCMC[ , t]   = delta
+      gamma_MCMC[ , t]   = gamma
+      A_MCMC[ , , t]     = A
+      tau_MCMC[ , t]     = tau
+      rho_MCMC[t]        = rho
+   }
+   
+   # return a list of MCMC samples
+   return(list(alpha = alpha_MCMC,
+               beta  = beta_MCMC,
+               delta = delta_MCMC,
+               gamma = gamma_MCMC,
+               A     = A_MCMC,
+               tau   = tau_MCMC,
+               rho   = rho_MCMC))
+}
